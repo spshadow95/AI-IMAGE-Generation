58c3efe32ee7f6eb05d95ed43ba9c655d7aabd9f
spshadow95 <126865454+spshadow95@users.noreply.github.com>
2023-10-07 16:17:52 +0530

    Update index.js about __dirname

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ import connectDB from './mongodb/connect.js';
 import postroutes from './routes/postroutes.js';
 import dalleroutes from './routes/dalleroutes.js';
 
-const path= import('path');
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+
+
+
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 
