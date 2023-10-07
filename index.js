f09fd704036e978f1e1ae7311a9a6787ea65251f
spshadow95 <pawarsahil989@gmail.com>
2023-10-07 15:32:59 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express  from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postroutes from './routes/postroutes.js';
+import dalleroutes from './routes/dalleroutes.js';
+
+const path= require('path');
+dotenv.config();
+
+
+const app= express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+app.use('/api/v1/post' , postroutes);
+app.use('/api/v1/dalle' , dalleroutes);
+
+app.use(express.static(path.join(__dirname, './client/dist')));
+
+
+app.get('*', async (req,res) => {
+    res.sendFile(path.join(__dirname , './client/dist/index.html'));
+});
+
+const startServer = async () => {
+    try{
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    }catch(error) {
+        console.log(error);
+    }
+
+
+    
+}
+
+startServer();
+
